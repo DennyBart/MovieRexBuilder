096fb76776be1d25b99784e42c655f6abc7ffef7
Denis B <59032775+nerdygremlin@users.noreply.github.com>
2023-06-18 16:05:20 +0200

    Add blurb generation

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ MOVIE_CRITIC_BOT_MESSAGE = 'You are a movie critic bot that responds with top mo
 GENERATE_MOVIE_RECOMMENDATION = 'Generate a list of interesting movie top list title recommendations. Like "Best Comedy Movies". ' \
     'Respond with the list in [] brackets title seperated with a comma'
 
-GENERATE_PAGE_BLOB = ''
+GENERATE_PAGE_BLURB = "You are a movie reviewer and you are writing a long summary intro to the best movies listed. Dont list the movies but combine in the summary"
 
 # This will trigger the omdb response of the plot of full or short - default is short if not set
 OMDB_PLOT = 'full'
