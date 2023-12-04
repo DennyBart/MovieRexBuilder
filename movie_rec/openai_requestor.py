309b57837fc5969f5cf6098c619151fc56a8cc15
Denis B <59032775+nerdygremlin@users.noreply.github.com>
2023-12-04 23:37:23 +0100

    Move DB Calls

--- movie_rec/openai_requestor.py
+++ movie_rec/openai_requestor.py
@@ -31,13 +31,7 @@ from movie_rec.movie_search import (
     set_posters_for_recommendation
 )
 import time
-
-# Replace with your own database URL
 DATABASE_URL = os.environ['DATABASE_URL']
-engine = create_engine(DATABASE_URL, pool_recycle=280)
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
 
 
 def create_movie_list(response):
@@ -114,6 +108,11 @@ def fetch_movie_details(movies, omdb_api_key, rec_topic=None):
 
 
 def store_movie_recommendation(movie_list, movie_type, total):
+    engine = create_engine(DATABASE_URL, pool_recycle=280)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
     unique_movie_list = list(set(movie_list))
 
     rec_uuid = str(uuid.uuid4())
@@ -326,6 +325,10 @@ def get_chatgpt_movie_rec(movie_type: str,
 
 
 def check_movie_recommendation(search_term=None, uuid=None, value=None):
+    engine = create_engine(DATABASE_URL, pool_recycle=280)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
     # Raise an exception if both search_term and uuid are None
     if search_term is None and uuid is None:
         raise ValueError(
@@ -363,6 +366,10 @@ def check_movie_recommendation(search_term=None, uuid=None, value=None):
 
 
 def get_related_movies(recommendation_uuid):
+    engine = create_engine(DATABASE_URL, pool_recycle=280)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
     # Query the relation table to get all associated movie UUIDs
     movie_relations = session.query(MovieRecommendationRelation) \
         .join(MovieData, MovieRecommendationRelation.
